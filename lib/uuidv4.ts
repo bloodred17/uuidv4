1a15faf96884bcad0a780fc29415d9dd341b6d9e
Ankur Dutta <ankur.611@gmail.com>
2022-07-18 21:48:14 +0530

    removed depeication warnings

--- lib/uuidv4.ts
+++ lib/uuidv4.ts
@@ -1,4 +1,3 @@
-import { deprecate } from 'util';
 import { NIL as nil, v4, v5, validate, version } from 'uuid';
 
 const regex = {
@@ -11,25 +10,13 @@ const jsonSchema = {
   v5: { type: 'string', pattern: regex.v5.toString().slice(1, -2) }
 };
 
-const uuidv4 = deprecate(
-  (): string => v4(),
-  'uuidv4() is deprecated. Use v4() from the uuid module instead.'
-);
+const uuidv4 = (): string => v4();
 
-const isUuid = deprecate(
-  (value: string): boolean => validate(value) && (version(value) === 4 || version(value) === 5),
-  'isUuid() is deprecated. Use validate() from the uuid module instead.'
-);
+const isUuid = (value: string): boolean => validate(value) && (version(value) === 4 || version(value) === 5);
 
-const empty = deprecate(
-  (): string => nil,
-  'empty() is deprecated. Use NIL from the uuid module instead.'
-);
+const empty = (): string => nil;
 
-const fromString = deprecate(
-  (text: string, namespace = 'bb5d0ffa-9a4c-4d7c-8fc2-0a7d2220ba45'): string => v5(text, namespace),
-  'fromString() is deprecated. Use v5() from the uuid module instead.'
-);
+const fromString = (text: string, namespace = 'bb5d0ffa-9a4c-4d7c-8fc2-0a7d2220ba45'): string => v5(text, namespace);
 
 export {
   uuidv4 as uuid,
